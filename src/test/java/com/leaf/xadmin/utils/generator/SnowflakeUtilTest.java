d6b6566d52b02e867a5f4fd92bfb0521e6ab5dc0
DESKTOP-A9HEVQA\leaf <806569552@qq.com>
2018-02-06 21:54:04 +0800

    测试文件目录更正

---
+++ src/test/java/com/leaf/xadmin/utils/generator/SnowflakeUtilTest.java
@@ -0,0 +1,26 @@
+package com.leaf.xadmin.utils.generator;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author leaf
+ * <p>date: 2018-01-13 15:34</p>
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class SnowflakeUtilTest {
+    @Test
+    public void nextId() throws Exception {
+        SnowflakeUtil idWorker = new SnowflakeUtil(0, 0);
+        for (int i = 0; i < 1; i++) {
+            long id = idWorker.nextId();
+            log.info(id + "");
+        }
+    }
+
+}
---
+++ src/test/java/com/leaf/xadmin/utils/request/RequestResolveUtilTest.java
@@ -0,0 +1,36 @@
+package com.leaf.xadmin.utils.request;
+
+import com.leaf.xadmin.controller.UserController;
+import com.leaf.xadmin.utils.request.RequestResolveUtil;
+import com.leaf.xadmin.vo.RequestResourceVO;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class RequestResolveUtilTest {
+
+    @Autowired
+    private RequestResolveUtil resolveUtil;
+
+    @Test
+    public void methodResolver() throws Exception {
+        resolveUtil.methodResolver(UserController.class.getMethod("getUser", String.class));
+    }
+
+    @Test
+    public void pathMerge() throws Exception {
+        List<RequestResourceVO> resourceList = resolveUtil.methodResolver(UserController.class.getMethod("getUser", String.class));
+        RequestResourceVO requestResourceVO = resourceList.get(0);
+        String pathMerge = RequestResolveUtil.pathMerge(requestResourceVO.getParentPath(), requestResourceVO.getChildPath());
+        log.debug(pathMerge);
+    }
+
+}
